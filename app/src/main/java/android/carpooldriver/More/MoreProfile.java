a0a2f8afd3f768df522fdf9a4579ac51203f1809
EdwardKHKim <kim.kyoohoon.edward@gmail.com>
2019-08-31 04:12:27 -0700

    Profile done

--- app/src/main/java/android/carpooldriver/More/MoreProfile.java
+++ app/src/main/java/android/carpooldriver/More/MoreProfile.java
@@ -1,4 +1,16 @@
 package android.carpooldriver.More;
 
-public class MoreProfile {
+import android.carpooldriver.R;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MoreProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
 }
