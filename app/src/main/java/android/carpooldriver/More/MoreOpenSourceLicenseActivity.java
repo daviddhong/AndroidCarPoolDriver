25ac2db24049aa9545e8e86ebbef1d83fdba146b
EdwardKHKim <kim.kyoohoon.edward@gmail.com>
2019-09-01 03:07:09 -0700

    buttons (found that some were not done)

--- app/src/main/java/android/carpooldriver/More/MoreOpenSourceLicenseActivity.java
+++ app/src/main/java/android/carpooldriver/More/MoreOpenSourceLicenseActivity.java
@@ -2,6 +2,8 @@ package android.carpooldriver.More;
 
 import android.carpooldriver.R;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +14,17 @@ public class MoreOpenSourceLicenseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_open_source_license);
+
+        initBack();
+    }
+
+    private void initBack() {
+        RelativeLayout back = (RelativeLayout) findViewById(R.id.rl_back_settings_open_source);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
