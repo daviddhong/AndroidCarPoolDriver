4f4f6353e8a9b3e58bb2345d1c8edc4f9ae7f65b
david hong <davidhong022@gmail.com>
2019-09-06 22:47:47 -0700

    added current user's name to settings to know who it is

--- app/src/main/java/android/carpooldriver/SettingsFragment.java
+++ app/src/main/java/android/carpooldriver/SettingsFragment.java
@@ -12,12 +12,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SettingsFragment extends Fragment {
 
@@ -25,11 +31,37 @@ public class SettingsFragment extends Fragment {
     private RelativeLayout sign_out_button;
     FirebaseAuth mAuth;
 
+    private DatabaseReference RootRef;
+    String currentUID;
+    private TextView firstName, lastName;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mMoreView = inflater.inflate(R.layout.fragment_more, container, false);
         mAuth = FirebaseAuth.getInstance();
+
+
+
+        firstName = mMoreView.findViewById(R.id.firstnameofuser);
+        lastName = mMoreView.findViewById(R.id.lastnameofuser);
+        currentUID = mAuth.getCurrentUser().getUid();
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.child("Users").child(currentUID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                firstName.setText(dataSnapshot.child("firstname").getValue().toString());
+                lastName.setText(dataSnapshot.child("lastname").getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
         initProfile();
         initEmail();
         initAccountPassword();
