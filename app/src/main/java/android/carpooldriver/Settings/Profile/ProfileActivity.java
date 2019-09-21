3212e0faf5c17d472d58411758a8998b66afb3fa
david hong <davidhong022@gmail.com>
2019-09-21 01:11:25 -0700

    settings fragment email, password, profile

--- app/src/main/java/android/carpooldriver/Settings/Profile/ProfileActivity.java
+++ app/src/main/java/android/carpooldriver/Settings/Profile/ProfileActivity.java
@@ -5,21 +5,63 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView Name, PhoneNumber, CAR;
+    private DatabaseReference UsersRef;
+    private String userUID;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        initFields();
+        setFields();
         initEditCar();
         initEditName();
         initEditPhoneNumber();
         initClose();
+    }
+
+    private void initFields() {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        userUID = mAuth.getCurrentUser().getUid();
+        Name = findViewById(R.id.name_profile);
+        PhoneNumber = findViewById(R.id.phone_number_profile);
+        CAR = findViewById(R.id.name_car_type);
+
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+    }
+
+    private void setFields() {
+        UsersRef.child(userUID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    Name.setText(dataSnapshot.child("firstname").getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     private void initEditCar() {
