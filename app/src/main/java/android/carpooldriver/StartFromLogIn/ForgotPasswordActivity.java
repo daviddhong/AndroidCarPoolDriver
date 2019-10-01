d6137182ad43887750e880842b48669b7bfe34d1
david hong <davidhong022@gmail.com>
2019-10-01 15:13:27 -0700

    forgot password activity working correctly

--- app/src/main/java/android/carpooldriver/StartFromLogIn/ForgotPasswordActivity.java
+++ app/src/main/java/android/carpooldriver/StartFromLogIn/ForgotPasswordActivity.java
@@ -31,34 +31,41 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         sendResetPasswordEmailButton = findViewById(R.id.button_reset_password);
         Email = findViewById(R.id.text_view_layout_email_reset);
 
-        forgotPassword();
+        forgotpassword();
     }
 
 
-    private void forgotPassword() {
-        String email = Email.getText().toString();
+    private void forgotpassword() {
 
         sendResetPasswordEmailButton.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mAuth.sendPasswordResetEmail(Email.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    Intent intent = new Intent(ForgotPasswordActivity.this, LogInActivity.class);
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                    startActivity(intent);
-                                    // EFFECTS: Animation from LogInActivity to BottomNavigationMainActivity.
-                                    ForgotPasswordActivity.this.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                                    Toast.makeText(ForgotPasswordActivity.this,
-                                            "Reset Password Email Sent To\n" + email, Toast.LENGTH_LONG).show();
-                                } else {
-                                    Toast.makeText(ForgotPasswordActivity.this,
-                                            "There is no account under the email:\n" + email, Toast.LENGTH_LONG).show();
+                String email = Email.getText().toString();
+                if (!(email.isEmpty())) {
+                    mAuth.sendPasswordResetEmail(Email.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    if (task.isSuccessful()) {
+
+                                        Intent intent = new Intent(ForgotPasswordActivity.this, LogInActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                        // EFFECTS: Animation from LogInActivity to BottomNavigationMainActivity.
+                                        ForgotPasswordActivity.this.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                                        Toast.makeText(ForgotPasswordActivity.this,
+                                                "Reset password email sent to\n" + email, Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(ForgotPasswordActivity.this,
+                                                "There is no account under the email:\n" + email, Toast.LENGTH_LONG).show();
+                                    }
                                 }
-                            }
-                        });
+                            });
+                } else {
+                    Toast.makeText(ForgotPasswordActivity.this,
+                            "Email is empty", Toast.LENGTH_LONG).show();
+                }
             }
         }));
     }
